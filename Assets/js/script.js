318cf30e2054132c026e7552666702a480f43128
unknown <droid64down@gmail.com>
2022-08-23 17:23:41 -0400

    Set up page formatting

---
+++ Assets/js/script.js
@@ -0,0 +1,34 @@
+//Steps for javascript code
+
+//Variable declarations
+    //Array containing questions with correct answer index
+let quesNAns = [
+    ["Test 1", 1],
+    ["Test 2", 1],
+    ["Test 3", 4],
+    ["Test 4", 1],
+    ["Test 5", 2],
+    ["Test 6", 3],
+    ["Test 7", 2],
+    ["Test 8", 4],
+    ["Test 9", 3],
+    ["Test 10", 1]
+    ];
+    //Array with Possible answers
+let posAns = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+
+//Function for reading DOM elements
+    //Will randomly choose a question from the array to display
+
+//Instantiate local storage for holding high score data
